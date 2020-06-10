3e0014f1d5c395086aaa6a1b70e6b7e27cad2769
therenow9 <60898774+therenow9@users.noreply.github.com>
2020-06-10 07:10:36 -0400

    Add files via upload

--- dat_converter.py
+++ dat_converter.py
@@ -5,26 +5,30 @@ Created on Jun 4, 2020
 '''
 # creates a new file in my python files directory
 import os, sys;
+import mysql.connector;
 # get get os stuff
 home = os.getcwd();
-# store hom
-og_dat_file="example.dat";
 #get name of dat file
-path = "/mnt/D/Documents/Programming/Pendant_Automation/Lucas_Docs/ASSIGNMENT-2020032214393416.DAT";
+path = "D:\Documents\Programming\Pendant_Automation\Lucas_Docs";
 # path of python documents fold
 os.chdir(path);
 # go to the directory
+# store hom
+og_dat_file="ASSIGNMENT-2020032214393416.DAT";
+#open file
 line_dump_data=[];
 #arrays to store the full line of text from the
 #dat file to then be modified with string functions and changed
 
-
 num_lines = sum(1 for line in open(og_dat_file));
 #get number of lines in the file
-for j in num_lines:
+for j in range(num_lines):
+    
     line_dump_data[j]=og_dat_file.readline(j);
+    print(line_dump_data[j]);
     #get raw data to be split up
     new_file_name="insert fancy string indexing function here on line dump data";
+    
     #get name for new dat file
     new_file_data="Insert more fancy string shit here to be added to the dat file";
     #get data to be added to the new dat file
