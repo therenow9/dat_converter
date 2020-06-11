fdec240b086edbbc26c3a2f97e4429c680450e84
therenow9 <60898774+therenow9@users.noreply.github.com>
2020-06-11 09:36:24 -0400

    Add files via upload

--- dat_converter.py
+++ dat_converter.py
@@ -3,53 +3,56 @@ Created on Jun 4, 2020
 
 @author: Jeremy Scheuerman
 '''
-# creates a new file in my python files directory
+
+# "D:\Documents\Programming\Pendant_Automation\Lucas_Docs";
+# /home/jeremy/Documents/Pendant_automation/Lucas_Docs
+
 import os, sys;
 import mysql.connector;
 # get get os stuff
-home = os.getcwd();
-#get name of dat file
-path = "D:\Documents\Programming\Pendant_Automation\Lucas_Docs";
-# path of python documents fold
-os.chdir(path);
-# go to the directory
-# store hom
-og_dat_file=open("ASSIGNMENT-2020032214393416.DAT","r");
-print(og_dat_file.readlines());
-#open file
-#dat file to then be modified with string functions and changed
+import logging;
+# import for debugging
 
-num_lines = sum(1 for line in og_dat_file);
-#get number of lines in the file
+working_path = "/home/jeremy/Documents/Pendant_automation/Lucas_Docs";
+# path of python documents fold
+save_path = "/home/jeremy/Documents/Pendant_automation/converter_tests";
+# path to save new files to
+os.chdir(working_path);
+# go to the directory
+home = os.getcwd();
+# store home
+orig_file_name = "ASSIGNMENT-2020032214393416.DAT";
+# get variable for file name 
+og_dat_file = open(orig_file_name, "r");
+# openfile
+all_lines = og_dat_file.readlines();
+# get read all lines variable
+num_lines = sum(1 for line in open(orig_file_name));
+# get number of lines in the file
+print("Number of files to be created "+str(num_lines));
+#print number of lines
 for j in range(num_lines):
+    line_dump_data = all_lines[j];
+    # get data from specific line 
+    new_file_name = line_dump_data[21:35]+".DAT";
+    # get name for new dat file from line data 
+    new_name_complete=os.path.join(save_path,new_file_name);
+    #and name combined with save path
+    new_file_data = line_dump_data;
+    # get data to be added to the new dat file
+    #print(new_file_name);
+    #print(new_file_data);
     
-    line_dump_data=og_dat_file.readline;
-    #print(line_dump_data);
-    #get raw data to be split up
-    
-    new_file_name="insert fancy string indexing function here on line dump data";
-    
-    #get name for new dat file
-    new_file_data="Insert more fancy string shit here to be added to the dat file";
-    #get data to be added to the new dat file
-    new_file = open(new_file_name, "x");
-    # Creates a new file from the one stored in the index of the array
-    
+    new_file = open(new_name_complete, "w");
+    # Creates a new file from the temp vars
+    new_file.write(new_file_data);
+    new_file.close();
+    '''
     with open(new_file, 'w') as fp: 
         pass
         # To write data to new file uncomment 
         fp.write(new_file_data);
-        
-
-#dat amount is the amount of .dat files in the list
-new_file_path = path  + name;
-# create variable with new file name;
-new_file = open(name, "x");
-# create new file
+        '''
 os.chdir(home);
-
 # return home
 
-  # fdsfds
-
-'''
